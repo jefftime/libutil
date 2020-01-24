16e739c48715010cb48d1926736347614b157e38
Jeffery Stager <jffry09@gmail.com>
2020-01-24 13:22:10 +0100

    license info

--- src/util.c
+++ src/util.c
@@ -1,3 +1,21 @@
+/* Copyright 2020, Jeffery Stager
+ *
+ * This file is part of libutil
+ *
+ * libutil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libutil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libutil.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 void u_puts(char *s) {
